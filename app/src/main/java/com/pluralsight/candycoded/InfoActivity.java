46d2452eccec9063fca68f0e00e9283ce37de29a
mas <mekontsoanderson@gmail.com>
2019-11-05 05:25:25 -0600

    Completed module

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,4 +43,10 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+
+    public void createPhoneIntent(View view) {
+        Intent dialerIntent = new Intent(Intent.ACTION_DIAL);
+        dialerIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(dialerIntent);
+    }
 }
